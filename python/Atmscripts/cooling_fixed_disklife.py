39524c89c15592104c7cd54b682a867d2ddc36f9
apiso <anamaria.piso@gmail.com>
2013-01-22 18:59:38 -0500

    added plot + scripts: Mcrit vs distance for disk life 3Myrs

--- python/Atmscripts/cooling_fixed_disklife.py
+++ python/Atmscripts/cooling_fixed_disklife.py
@@ -1,19 +1,34 @@
 
 
-a = numpy.linspace(10, 100, 10)
 
 from RadSGPoly.atmseries_poly import mcrit_disktime as mcd
 
 a = 10
+exec_file = 0
+save = 1
 
-Mcrit = 0 * numpy.ndarray(shape = (10), dtype = float)
+if exec_file == 1:
 
-for k in range(10):
-    execfile('coolingtime_plots_sg.py')
-    Mcrit[k] = mcd(Mc, t)
-    a = a + 10
+    Mcrit00 = 0 * numpy.ndarray(shape = (10), dtype = float)
+    Mcrit03 = 0 * numpy.ndarray(shape = (10), dtype = float)
 
+    for k in range(10):
+        Y = 0.0
+        execfile('coolingtime_plots_sg.py')
+        Mcrit00[k] = mcd(Mc, t)
+        
+        Y = 0.3
+        execfile('coolingtime_plots_sg.py')
+        Mcrit03[k] = mcd(Mc, t)
+
+        a = a + 10
     
 au = numpy.linspace(10, 100, 10)
-plt.semilogx(au, Mcrit, '-o')
+plt.semilogx(au, Mcrit00, '-o', label = r'$\nabla_{\mathrm{ad}}=2/7$, $Y=0.0$')
+plt.semilogx(au, Mcrit03, '-rs', label = r'$\nabla_{\mathrm{ad}}=2/7$, $Y=0.3$')
+plt.legend()
+plt.title(r'Critical core mass vs. distance for disk lifetime of 3 Myrs')
+if save == 1:
+    plt.savefig(userpath + \
+                '/figs/ModelAtmospheres/RadSelfGravPoly/Mcrit_vs_a_3Myrs.pdf')
 plt.show()
--- python/Atmscripts/coolingtime_plots_sg.py
+++ python/Atmscripts/coolingtime_plots_sg.py
@@ -14,13 +14,16 @@ from RadSGPoly.cooling_poly import cooling_global as cg, cooling_local as cl, cr
 os.chdir(userpath + '/python/Atmscripts')
 
 delad = 2./7
-Y = 0.3
+
 
 load = 1
 cool = 1
 savefig = 0
+nofixeddisk = 1
 
-#a = 10.0
+if nofixeddisk == 0:
+    Y = 0.0
+    a = 100.0
 
 prms = params(Me, Re, a, delad, Y, gamma = gammafn(delad), R = Rfn(Y), \
               Cv = Cvfn(Y, delad), Pd = Pdisk(a, mstar, FSigma, FT), \
@@ -28,19 +31,35 @@ prms = params(Me, Re, a, delad, Y, gamma = gammafn(delad), R = Rfn(Y), \
 
 if load != 0:
 
-    atm5 = atmload('Mc1.0.npz', prms = prms)
-    atm6 = atmload('Mc2.0.npz', prms = prms)
-    atm7 = atmload('Mc3.0.npz', prms = prms)
-    atm8 = atmload('Mc4.0.npz', prms = prms)
-    atm9 = atmload('Mc5.0.npz', prms = prms)
-    atm10 = atmload('Mc6.0.npz', prms = prms)
-    atm11 = atmload('Mc7.0.npz', prms = prms)
-    atm12 = atmload('Mc8.0.npz', prms = prms)
-    atm13 = atmload('Mc9.0.npz', prms = prms)
-    atm14 = atmload('Mc10.0.npz', prms = prms)
-    #atm15 = atmload('Mc13.0.npz', prms = prms)
+    if Y == 0.3:
 
-Mc = numpy.linspace(1, 10, 10)
+        atm5 = atmload('Mc1.0.npz', prms = prms)
+        atm6 = atmload('Mc2.0.npz', prms = prms)
+        atm7 = atmload('Mc3.0.npz', prms = prms)
+        atm8 = atmload('Mc4.0.npz', prms = prms)
+        atm9 = atmload('Mc5.0.npz', prms = prms)
+        atm10 = atmload('Mc6.0.npz', prms = prms)
+        atm11 = atmload('Mc7.0.npz', prms = prms)
+        atm12 = atmload('Mc8.0.npz', prms = prms)
+        atm13 = atmload('Mc9.0.npz', prms = prms)
+        atm14 = atmload('Mc10.0.npz', prms = prms)
+
+        Mc = numpy.linspace(1, 10, 10)
+
+    elif Y == 0.0:
+        
+        atm5 = atmload('Mc5.0.npz', prms = prms)
+        atm6 = atmload('Mc6.0.npz', prms = prms)
+        atm7 = atmload('Mc7.0.npz', prms = prms)
+        atm8 = atmload('Mc8.0.npz', prms = prms)
+        atm9 = atmload('Mc9.0.npz', prms = prms)
+        atm10 = atmload('Mc10.0.npz', prms = prms)
+        atm11 = atmload('Mc11.0.npz', prms = prms)
+        atm12 = atmload('Mc12.0.npz', prms = prms)
+        atm13 = atmload('Mc13.0.npz', prms = prms)
+        atm14 = atmload('Mc14.0.npz', prms = prms)       
+
+        Mc = numpy.linspace(5, 14, 10)
 
 model = [atm5[0], atm6[0], atm7[0], atm8[0], atm9[0], atm10[0], atm11[0], atm12[0], atm13[0], atm14[0]]
 param = [atm5[1], atm6[1], atm7[1], atm8[1], atm9[1], atm10[1], atm11[1], atm12[1], atm13[1], atm14[1]]
@@ -62,7 +81,7 @@ plt.axhline(y = 3, linestyle = '--', color = 'black', label = 't = 3 Myrs')
 plt.xlabel(r'$M_c (M_{\bigoplus})$ ')
 plt.ylabel(r'$t_{\mathrm{cool}}$(Myrs)')
 plt.legend()
-plt.title('a=' + str(int(a)) + ' AU')
+plt.title(r'$\nabla=$' + str(delad) + ', Y=' + str(Y)[:4] + ', a=' + str(int(a)) + ' AU')
 if savefig != 0:
     plt.savefig(userpath + '/figs/ModelAtmospheres/RadSelfGravPoly/coolingtime_vs_Mc_' + str(int(a)) + 'au.pdf')
 plt.show()
