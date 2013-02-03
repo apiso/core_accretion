421f758812c966ab58e4265a9957d5a0ca854d8a
Andrew Youdin <youdin@work>
2013-02-02 20:51:26 -0700

    modified analytic comparisons
    I did this as an addition not a subtraction.  So feel free to clean up the plots_poster notebook after you look at my changes, which I'm hoping I can merge.  For the analytic model I added a (numerical) computation of the radiative mass for a fairer comparison.  Now the agreement at early times is good at least.  I did change so that L_cb depends only on core mass which is what the analytic calculation actually does. This made no visible difference.

--- python/Analytical/analytic_comp.py
+++ python/Analytical/analytic_comp.py
@@ -7,12 +7,16 @@ import numpy as np
 from utils.constants import G, kb, mp, Rb, Me, Re, Msun, RH, RHe, sigma, \
      cmperau, RHill, gammafn, mufn, Rfn, Cvfn, kdust, Tdisk, Pdisk, params
 from utils.parameters import mstar, FSigma, FT
+from scipy.integrate import odeint
 
 def analytic_sols(delad, Y, rhoc, a, Mc, Pcb, beta = 2, theta = 0.556):
 
     """
     Computes analytic estimates for a given set of gas, disk and core conditions
     and a given value (or set of values) at the radiative convective boundary.
+
+    WARNING: Though this function takes delad as a parameter, the results for
+    cooling time and M_CB assume delad = 2./7.  This should be changed.
 
     Input
     -----
@@ -44,11 +48,12 @@ def analytic_sols(delad, Y, rhoc, a, Mc, Pcb, beta = 2, theta = 0.556):
         
     
     """
-    
+
+    p4 = 4 * np.pi
     delinf = 1. / (4 - beta)
 
     rc = (3 * Mc / (4 * np.pi * rhoc))**(1./3)
-
+    
     R = Rfn(Y)
 
     Td = Tdisk(a, FT)
@@ -56,21 +61,71 @@ def analytic_sols(delad, Y, rhoc, a, Mc, Pcb, beta = 2, theta = 0.556):
 
     chi = (1 - delad / delinf)**(-1. / (4 - beta))
     Tcb = Td * chi
-    RBprime = (delad / chi) * (G * Mc / (R * Td))
+    RB = G * Mc / (R * Td)
+    RBprime = (delad / chi) * RB
     PM = (4 * delad**(3./2) / (5 * np.pi**2 * np.sqrt(chi))) * (\
         G * Mc**2 / RBprime**4)
 
-    Matm = Mc * (Pcb / PM) / (np.sqrt(np.log(Pcb / (theta * Pd))))
+    Mcb = Mc * (Pcb / PM) / (np.sqrt(np.log(Pcb / (theta * Pd))))
 
-    Lo = 64 * np.pi * G * (Mc + Matm) * sigma * Td**4 /\
+    """While the true L at convective zone depends on Mcb, the analytic
+    theory only considers Mc to make integrals easier"""
+    ML = Mc #+ Mcb neglected
+    Lo = 64 * np.pi * G * ML * sigma * Td**4 /\
                (3 * kdust(Td) * Pd) * delad * chi**(4 - beta)
 
     Lcb = Lo * Pd / Pcb
 
     tcool = 4 * np.pi * Pcb**2 / Pd * RBprime**(7./2) / (Lo * np.sqrt(rc))
 
-    return Tcb, Matm, Lcb, tcool / (365 * 24 * 3600)
+    #Now add correction to atmospheric mass for radiative zone mass.
+    #Alternately the comparison of numerical and analytic could be done at
+    #RCB
 
+    rcb = RB / np.log(Pcb/(theta*Pd))
+    """Below is only a weakly self-gravitating Hill radius as the radiative
+    zone mass is excluded, and further `Mcb` is from a non-self-grav. calc.
+    Good enough for our purposes here."""
+    rHill = RHill(Mc + Mcb, a, Ms=mstar*Msun)
+         
+    def mPstruct(y, r, Tfunc):
+         """HB struct equations requiring `Tfunc` = T(P).
+         Putting as sub-fn for now to avoid passing params."""
+
+         m, P = y
+         T = Tfunc(P)
+         rho = P / R / T
+         dmdr = p4 * rho * r**2
+         dPdr = - rho * G * m / r**2
+         return dmdr, dPdr
+
+    def TPrad(P):
+         """T-P relation that requires setting Pcb value outside fn
+         and knowing Pd.  Classes are DEFINITELY the better way to do
+         this, but we're not there yet."""
+
+         myPcb = TPrad.Pcb
+         #WARNING: in this form the temp is specific to alpha = 0,
+         #         no pressure dependence in powerlaw opacity
+         Tfac = (1 + (P - Pd) / myPcb / (delinf/delad - 1))**delinf
+         return Td * Tfac
+
+    #if only a single `Pcb` is passed make quantities iterable
+    if not hasattr(Pcb, '__iter__'):
+          Pcb = [Pcb]
+          rcb, Mcb, rHill = [rcb], [Mcb], [rHill]
+
+    Mrad = np.empty_like(Pcb) 
+    for i, Pcb_i in enumerate(Pcb):
+         TPrad.Pcb = Pcb_i
+         y = odeint(mPstruct, (Mc + Mcb[i], Pcb_i), (rcb[i], RB, rHill[i]),
+                    args=(TPrad,))
+         Mtot = y[1, 0] #for row entry (first) use 1 for RB mass
+                        #and 2 (or -1) for Hill
+         Mrad[i] = Mtot - (Mc + Mcb[i])
+    
+    return Tcb, Mcb, Lcb, tcool / (365 * 24 * 3600), rcb, Mrad, rHill
+    #if desired could switch Mcb to total Matm above.
 
 
 
