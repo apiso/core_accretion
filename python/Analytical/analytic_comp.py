3f829f6a3879e9f3578e696b74338d5d96760329
Andrew Youdin <youdin@work>
2013-02-19 17:30:06 -0700

    analytic model and plots added
    for comparison to  temp and pressure effect and Mcrit dependence including kappa and mu.  As python class.

--- python/Analytical/analytic_comp.py
+++ python/Analytical/analytic_comp.py
@@ -3,11 +3,17 @@ abalytic model does not take into account self-gravity in either the convective
 or radiative region.
 Useful for comparison with the full numerical model."""
 
+import sys
 import numpy as np
 from utils.constants import G, kb, mp, Rb, Me, Re, Msun, RH, RHe, sigma, \
-     cmperau, RHill, gammafn, mufn, Rfn, Cvfn, kdust, Tdisk, Pdisk, params
-from utils.parameters import mstar, FSigma, FT
+     cmperau, RHill, gammafn, mufn, Rfn, Cvfn, kdust, Tdisk, Pdisk, params, \
+     Myr
+from utils.parameters import mstar, FSigma, FT, rhoc
+from utils.mynumsci import zbrac
 from scipy.integrate import odeint
+from scipy.optimize import brentq
+
+p4 = 4 * np.pi
 
 def analytic_sols(Pcb, delad, Y, rhoc, a, Mc, beta = 2, theta = 0.556):
 
@@ -49,7 +55,6 @@ def analytic_sols(Pcb, delad, Y, rhoc, a, Mc, beta = 2, theta = 0.556):
     
     """
 
-    p4 = 4 * np.pi
     delinf = 1. / (4 - beta)
 
     rc = (3 * Mc / (4 * np.pi * rhoc))**(1./3)
@@ -128,7 +133,104 @@ def analytic_sols(Pcb, delad, Y, rhoc, a, Mc, beta = 2, theta = 0.556):
     #if desired could switch Mcb to total Matm above.
 
 
+class AnSols:
+    p4 = 4 * np.pi
+    beta = 2
+    delad = 2./7
 
+    delinf = 1. / (4 - beta)
+    chi = (1 - delad / delinf)**(-1. / (4 - beta))
+    theta = 0.556
+
+    #Mcb = Mc * (Pcb / PM) / (np.sqrt(np.log(Pcb / (theta * Pd))))
+
+    """While the true L at convective zone depends on Mcb, the analytic
+    theory only considers Mc to make integrals easier"""
+    #ML = Mc #+ Mcb neglected
+    #Lo = 64 * np.pi * G * ML * sigma * Td**4 /\
+    #           (3 * kdust(Td) * Pd) * delad * chi**(4 - beta)
+
+    #Lcb = Lo * Pd / Pcb
+
+    def __init__(self, mu=2.35, a=10, FT=FT, **kwargs):
+        #disk
+        self.Td = kwargs.get('Td', Tdisk(a, FT))
+        self.Pd = kwargs.get('Pd', Pdisk(a, mstar, FSigma, FT))
+        self.a = a #for checking
+
+        #thermodyn & atmos.
+        self.R = kb / mp / mu
+        self.Tcb = self.Td * self.chi
+        self.FK = kwargs.get('FK', 1)
+
+    def setcore(self, mc=5, **kwargs):
+        #core mass and radius
+        self.mc = mc
+        self.Mc = self.mc * Me
+        self.rhoc = kwargs.get('rhoc', rhoc)
+        self.rc = (3 * self.Mc / (4 * np.pi * self.rhoc))**(1./3)
+
+        Mc, Td, Pd, R = self.Mc, self.Td, self.Pd, self.R
+        delad, chi, beta = self.delad, self.chi, self.beta
+        self.RB = G * Mc / (R * Td)
+        self.RBprime = (delad / chi) * self.RB
+        self.PM = (4 * delad**(3./2) / (5 * np.pi**2 * np.sqrt(chi))) * (\
+            G * Mc**2 / self.RBprime**4)
+        self.Lo = 64 * np.pi * G * Mc * sigma * Td**4 /\
+            (3 * self.FK * kdust(Td) * Pd) * delad * chi**(4 - beta)
+
+    def tcool(self, mc=5, fM=0.15, **kwargs):
+        """returns cooling time in Myr"""
+
+        self.setcore(mc, **kwargs)
+        PM, Pd = self.PM, self.Pd
+        fac = fM * PM / (self.theta * Pd)
+        ksi = self.findksi(fac)
+        #print "fac = {:.2f}, ksi = {:.2f}".format(fac, ksi)
+        Pcb = ksi * fM * PM
+
+        tcool = p4 * Pcb**2 / Pd * self.RBprime**(7./2) / (
+            self.Lo * np.sqrt(self.rc))
+
+        return tcool/Myr
+
+    def findksi(self, fac):
+        def ksiroot(ksi, fac):
+            return ksi - np.sqrt(np.log(ksi * fac))
+
+        if fac < 2.3317:
+            print "no ksi root"
+            return 0
+        (k0, k1), success = zbrac(ksiroot, np.sqrt(2), 2, args=(fac,))
+        if success == False:
+            sys.exit('ksi bracket not found')
+        return brentq(ksiroot, k0, k1, args = (fac,))
+
+
+    def mcrit(self, mg, tdisk=3, fM=0.15, **kwargs):
+        """
+        find root of tdisk = tcool
+
+        Input
+        -----
+        mg -- two element
+            initial mass guesses (in Earth masses)
+        tdisk -- number
+            disk lifetime in Myr
+        """
+
+        def mcrit_root(mc, tdisk, fM):
+            return tdisk - self.tcool(mc, fM)
+
+        f = mcrit_root
+        (m0, m1), success = zbrac(f, mg[0], mg[1], args=(tdisk, fM))
+        if success == False:
+            sys.exit('mcrit bracket not found')
+        return brentq(f, m0, m1, args = (tdisk, fM))
+        
+
+
+    
 
 
 
